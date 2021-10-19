aa7d7cc0c2bfa8a4b20fdd3c3f8448ed0eb17327
Monika Borowiak <m.borowiak91@gmail.com>
2021-10-19 20:28:19 +0100

    websocket-for-one-user

---
+++ index.js
@@ -0,0 +1,15 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+  });
+
+  ws.send("something");
+
+  setTimeout(() => {
+    ws.send("n");
+  }, 20000);
+});
